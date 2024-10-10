fd4188c0efbb6b85b54da977222ba68056b902ae
Banggi Bima <banggibima@gmail.com>
2024-10-10 19:52:57 +0700

    refactor: refactoring migration and middleware

--- internal/transport/middleware/jwt_middleware.go
+++ internal/transport/middleware/jwt_middleware.go
@@ -8,7 +8,9 @@ type JWTMiddleware struct {
 	Config config.Config
 }
 
-func NewJWTMiddleware(config config.Config) JWTMiddleware {
+func NewJWTMiddleware(
+	config config.Config,
+) JWTMiddleware {
 	return JWTMiddleware{
 		Config: config,
 	}
